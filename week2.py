d58f582a04cf7ec49abd65c568a598b02121d5de
Branch <dlbranch@cps.k12.il.us>
2024-09-04 15:05:56 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,20 @@
+# ask someone what day it is
+
+# ask them for their favorite holiday
+
+#print out the day and their favorite holiday in a sentece
+
+# making a chnage to the file
+
+day = input("What day is it today?")
+print("Today is " + day)
+
+holiday = input("What is you favorite holiday?")
+print(" and your favorite holdiday is " + holiday)
+
+print("Today is " , day ,  " and your favorite holdiday is "  + holiday)
+
+# making a change top the file
+# string sl;icing
+word = "hello"
+print(word[1:4])
